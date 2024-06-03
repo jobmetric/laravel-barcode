b6a87caf74c4ba9479c523207a8a423cf3f30be3
majid mohammadian <majeedmohammadian@gmail.com>
2024-06-03 19:00:06 +0330

    add language file

---
+++ lang/en/base.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Barcode Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Barcode for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    'validation' => [
+        'errors' => 'Validation errors occurred.',
+        'barcode_not_found' => 'The barcode not found.',
+    ],
+
+    'messages' => [
+        'created' => 'The barcode was created successfully.',
+        'updated' => 'The barcode was updated successfully.',
+        'deleted' => 'The barcode was deleted successfully.',
+    ],
+
+];
