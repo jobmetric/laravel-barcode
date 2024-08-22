b6fab9d68ee2fac28978843f2f3dc0dd3bcb5f2e
majid mohammadian <majeedmohammadian@gmail.com>
2024-08-22 23:01:31 +0330

    change comment in barcode resource

--- src/Http/Resources/BarcodeResource.php
+++ src/Http/Resources/BarcodeResource.php
@@ -12,6 +12,7 @@ use Illuminate\Http\Resources\Json\JsonResource;
  * @property mixed type
  * @property mixed value
  * @property mixed created_at
+ *
  * @property mixed barcodeable_resource
  */
 class BarcodeResource extends JsonResource
